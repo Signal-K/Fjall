50720248004f8fbe64d1394a552006b336a18cf0
Gizmotronn <droidology@allianceofdroids.org.au>
2022-03-13 13:07:05 +0800

    #1 Added alchemy keys for server auth

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+// Add an address to a notificatio in Alchemy
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+async function addAddress(new_address) {
+  console.log("adding address " + new_address);
+  const body = { webhook_id: "160886", addresses_to_add: ["0xab5801a7d398351b8be11c439e05c5b3259aec9b"], addresses_to_remove: [] };
+  try {
+    fetch('http://dashboard.alchemyapi.io/api/update-webhook-addresses', {
+      method: 'PATCH',
+      body: JSON.stringify(body),
+      headers: { 'Content-Type': 'application/json' },
+      headers: { 'X-Alchemy-Token': "MQGAOmHJhGwApszvpiqcF4sL9-hRRwmx" }
+    })
+      .then(res => res.json())
+      .then(json => console.log(json));
+  }
+  catch (err) {
+    console.error("An error occured: " + err);
+  }
+}

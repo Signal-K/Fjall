fe50388abb0f686b392d95dee1ef58efb1141bad
Gizmotronn <droidology@allianceofdroids.org.au>
2022-03-13 13:06:29 +0800

    #1 Added alchemy keys for server auth

--- test.js
+++ test.js
@@ -1,28 +1,19 @@
-const { spawn } = require('child_process');
-const got = require('got');
-const test = require('tape');
+// Add an address to a notificatio in Alchemy
 
-// Start the app
-const env = Object.assign({}, process.env, {PORT: 5000});
-const child = spawn('node', ['index.js'], {env});
-
-test('responds to requests', (t) => {
-  t.plan(4);
-
-  // Wait until the server is ready
-  child.stdout.on('data', _ => {
-    // Make a request to our app
-    (async () => {
-      const response = await got('http://127.0.0.1:5000');
-      // stop the server
-      child.kill();
-      // No error
-      t.false(response.error);
-      // Successful response
-      t.equal(response.statusCode, 200);
-      // Assert content checks
-      t.notEqual(response.body.indexOf("<title>Node.js Getting Started on Heroku</title>"), -1);
-      t.notEqual(response.body.indexOf("Getting Started on Heroku with Node.js"), -1);
-    })();
-  });
-});
+async function addAddress(new_address) {
+  console.log("adding address " + new_address);
+  const body = { webhook_id: "160886", addresses_to_add: ["0xab5801a7d398351b8be11c439e05c5b3259aec9b"], addresses_to_remove: [] };
+  try {
+    fetch('http://dashboard.alchemyapi.io/api/update-webhook-addresses', {
+      method: 'PATCH',
+      body: JSON.stringify(body),
+      headers: { 'Content-Type': 'application/json' },
+      headers: { 'X-Alchemy-Token': "MQGAOmHJhGwApszvpiqcF4sL9-hRRwmx" }
+    })
+      .then(res => res.json())
+      .then(json => console.log(json));
+  }
+  catch (err) {
+    console.error("An error occured: " + err);
+  }
+}

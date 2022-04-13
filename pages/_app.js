68a33749554057f29fcd3c9158910e9036dad24a
Gizmotronn <droidology@allianceofdroids.org.au>
2022-04-13 17:49:18 +0800

    Authentication completed for prev commit #1

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,16 @@
 import {ThirdwebWeb3Provider} from "@3rdweb/hooks";
 
+const supportedChainIds = [1, 4] // mainnet and rinkeby
+const connectors = {
+  injected: {}
+}
+
 function MyApp({ Component, pageProps }) {
   return (
-    <ThirdwebWeb3Provider>
+    <ThirdwebWeb3Provider
+      supportedChainIds={supportedChainIds}
+      connectors={connectors}
+    >
       <Component {...pageProps} />
     </ThirdwebWeb3Provider>
   )
